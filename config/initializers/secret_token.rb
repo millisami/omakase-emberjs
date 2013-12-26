3797c04be670c9e028ac3a9f7c9095f7d69ccf2f
Millisami <millisami@gmail.com>
2013-12-26 11:56:04 +0545

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omakase::Application.config.secret_key_base = 'b1b3e2fcfe5d0f7c8e5b905b7f804039dd1879616854a30737ce4a295a73c957feda37bd616416659b6d8a0c8a909587ea729b6d727cd716b115bdd317e1486d'
