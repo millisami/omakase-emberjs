2ff3bb97e6b8fb5e8913ca56455174d6640e93c8
Millisami <millisami@gmail.com>
2014-01-02 15:03:19 +0545

    Gems installed, devise setup, session controllers and helper methods added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
+  respond_to :json
+  before_action :default_json
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  def current_user
+    return nil unless params[:auth_token]
+    User.find_by authentication_token: params[:auth_token]
+  end
+
+protected
+  def default_json
+    request.format = :json if params[:format].nil?
+  end
+
+  def auth_only!
+    render json: {}, status: 401 unless current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Omakase::Application.routes.draw do
+
+  devise_for :users, only: []
+
+  devise_scope :user do
+    post    'sign_in' => 'sessions#create'
+    delete  'sign_out' => 'sessions#destroy'
+  end
+
   resources :comments
 
   resources :stories
